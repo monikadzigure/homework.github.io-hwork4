e61e3a353c2e397a461506b8a20f876851bb0ee5
monikadzigure <60093620+monikadzigure@users.noreply.github.com>
2020-03-02 02:08:00 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,130 @@
+let todoItems = [];
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+
+  const list = document.querySelector('.js-todo-list');
+  list.insertAdjacentHTML('beforeend', `
+    <li class="todo-item" data-key="${todo.id}">
+      <input id="${todo.id}" type="checkbox"/>
+      <label for="${todo.id}" class="tick js-tick"></label>
+      <span>${todo.text}</span>
+      <button class="delete-todo js-delete-todo">
+        <svg><use href="#delete-icon"></use></svg>
+      </button>
+    </li>
+  `);
+}
+
+function toggleDone(key) {
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  todoItems[index].checked = !todoItems[index].checked;
+
+  const item = document.querySelector(`[data-key='${key}']`);
+  if (todoItems[index].checked) {
+    item.classList.add('done');
+  } else {
+    item.classList.remove('done');
+  }
+}
+
+function deleteTodo(key) {
+  todoItems = todoItems.filter(item => item.id !== Number(key));
+  const item = document.querySelector(`[data-key='${key}']`);
+  item.remove();
+}
+
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const input = document.querySelector('.js-todo-input');
+
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
+
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+
+  if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+
+});
+
+
+
+
+var vid = "bpt84ceWAY0",
+    audio_streams = {},
+    audio_tag = document.getElementById('youtube');
+
+fetch("https://"+vid+"https://www.youtube.com/watch?v=-ESQmzDbnL8" + vid).then(response => {
+    if (response.ok) {
+        response.text().then(data => {
+
+            var data = parse_str(data),
+                streams = (data.url_encoded_fmt_stream_map + ',' + data.adaptive_fmts).split(',');
+
+            streams.forEach(function(s, n) {
+                var stream = parse_str(s),
+                    itag = stream.itag * 1,
+                    quality = false;
+                console.log(stream);
+                switch (itag) {
+                    case 139:
+                        quality = "48kbps";
+                        break;
+                    case 140:
+                        quality = "128kbps";
+                        break;
+                    case 141:
+                        quality = "256kbps";
+                        break;
+                }
+                if (quality) audio_streams[quality] = stream.url;
+            });
+
+            console.log(audio_streams);
+
+            audio_tag.src = audio_streams['128kbps'];
+            audio_tag.play();
+        })
+    }
+});
+
+function parse_str(str) {
+    return str.split('&').reduce(function(params, param) {
+        var paramSplit = param.split('=').map(function(value) {
+            return decodeURIComponent(value.replace('+', ' '));
+        });
+        params[paramSplit[0]] = paramSplit[1];
+        return params;
+    }, {});
+}
+
+function myFunction() {
+  alert("“I hated every minute of training, but I said, ‘Don’t quit. Suffer now and live the rest of your life as a champion.” – Muhammad Ali");
+
+
+
+
+
+}
+
+
